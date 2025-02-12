90af8cbefb6bfbafdc449a08354f803163c52ade
neha_khule <neha.khule@vernost.com>
2025-02-12 12:34:05 +0530

    Add tests for string calculator: handle new lines, different delimiters, and negative numbers

--- test/string_calculator_project_test.dart
+++ test/string_calculator_project_test.dart
@@ -15,4 +15,33 @@ void main() {
   test('Two numbers separated by comma', () {
     expect(calculator.add("1,5"), equals(6));
   });
+  test('Handle new lines between numbers', () {
+    expect(calculator.add("1\n2,3"), equals(6));
+  });
+
+  test('Support different delimiters', () {
+    expect(calculator.add("//;\n1;2"), equals(3));
+  });
+
+  test('Throw exception for single negative number', () {
+    expect(
+        () => calculator.add("-1"),
+        throwsA(predicate((e) =>
+            e.toString() == "Exception: negative numbers not allowed -1")));
+  });
+
+  test('Throw exception for multiple negative numbers', () {
+    expect(
+        () => calculator.add("1,-2,3,-4"),
+        throwsA(predicate((e) =>
+            e.toString() == "Exception: negative numbers not allowed -2, -4")));
+  });
+
+  test('Throw exception when all numbers are negative', () {
+    expect(
+        () => calculator.add("-1,-2,-3"),
+        throwsA(predicate((e) =>
+            e.toString() ==
+            "Exception: negative numbers not allowed -1, -2, -3")));
+  });
 }
